1a43133cce3e4adb77257165a703fc2e54aaba27
unknown <michaljaroch@hotmail.com>
2025-02-02 16:22:55 +0000

    live status

--- app.py
+++ app.py
@@ -4,23 +4,24 @@ import pandas as pd
 import json
 import pydeck
 from station_status import *
-with open('station_information.json') as json_file:
-    
-    data = json.load(json_file)
-    data_frame = {"lat": [], "lon": [], "name": [], "capacity": [], "station_id": []}
-    for station in data["data"]["stations"]:
-        data_frame["lat"].append(station["lat"])
-        data_frame["lon"].append(station["lon"])
-        data_frame["name"].append(station["name"])
-        data_frame["capacity"].append(station["capacity"])
-        data_frame["station_id"].append(station["station_id"])
+
+url = "https://gbfs.lyft.com/gbfs/2.3/chi/en/station_information.json"
+response = requests.get(url)
+data = response.json()
+data_frame = {"lat": [], "lon": [], "name": [], "capacity": [], "station_id": []}
+for station in data["data"]["stations"]:
+    data_frame["lat"].append(station["lat"])
+    data_frame["lon"].append(station["lon"])
+    data_frame["name"].append(station["name"])
+    data_frame["capacity"].append(station["capacity"])
+    data_frame["station_id"].append(station["station_id"])
 df1 = pd.DataFrame(data_frame)
 
 status_data = status()
 df2 = pd.DataFrame(status_data)  
 
 data_frame = df1.merge(df2, on="station_id", how="left")        
-#st.map(map_data)
+
 map_data = pd.DataFrame(data_frame)
 
 point_layer = pydeck.Layer(
@@ -46,10 +47,10 @@ view_state=pydeck.ViewState(
 chart = pydeck.Deck(
     point_layer,
     initial_view_state=view_state,
-    tooltip={"text": "{name}, \nCapacity: {capacity}, \nFree bikes: {bikes}, \nFree e-bikes: {ebikes}, \nFree scooters: {scooters}"},
+    tooltip={"text": "{name}, \nCapacity: {capacity}, \nAvailable bikes: {bikes}, \nAvailable e-bikes: {ebikes}, \nAvailable scooters: {scooters}"},
 )
 
-#st.pydeck_chart(chart)
+
 event = st.pydeck_chart(chart, on_select="rerun", selection_mode="multi-object")
 
 event.selection
