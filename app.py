12971706e252a8383efa9d45ce126cafad358551
unknown <michaljaroch@hotmail.com>
2025-02-17 22:23:46 +0000

    Add return map

--- app.py
+++ app.py
@@ -5,66 +5,93 @@ import json
 import pydeck
 from station_status import *
 
-def calculate_color(row):
-    base = row["capacity"]
-    percentage = (row["bikes"] + row["ebikes"] + row["scooters"]) / base * 100  
+def calculate_color(row, type):
+        
+    if type == "rent":
+        base = row["capacity"]
+        percentage = (row["bikes"] + row["ebikes"] + row["scooters"]) / base * 100  
+        
+        # Assign color based on percentage thresholds
+        if percentage > 50:
+            return [0, 255, 0]    # Green
+        elif int(percentage) == 0:
+            return [255, 0, 0]    # Red
+        elif percentage < 50:
+            return [255, 165, 0]  # Orange
+    elif type == "return":
+        if row["return"] == 1:
+            return [0, 255, 0]
+        else:
+            return [255, 0, 0]
     
-    # Assign color based on percentage thresholds
-    if percentage > 50:
-        return [0, 255, 0]    # Green
-    elif int(percentage) == 0:
-        return [255, 0, 0]    # Red
-    elif percentage < 50:
-        return [255, 165, 0]  # Orange
+def station_info():
+
+    url = "https://gbfs.lyft.com/gbfs/2.3/chi/en/station_information.json"
+    response = requests.get(url)
+    data = response.json()
+    data_frame = {"lat": [], "lon": [], "name": [], "capacity": [], "station_id": []}
+    for station in data["data"]["stations"]:
+        data_frame["lat"].append(station["lat"])
+        data_frame["lon"].append(station["lon"])
+        data_frame["name"].append(station["name"])
+        data_frame["capacity"].append(station["capacity"])
+        data_frame["station_id"].append(station["station_id"])
     
+    return data_frame
 
 
-
-
-url = "https://gbfs.lyft.com/gbfs/2.3/chi/en/station_information.json"
-response = requests.get(url)
-data = response.json()
-data_frame = {"lat": [], "lon": [], "name": [], "capacity": [], "station_id": []}
-for station in data["data"]["stations"]:
-    data_frame["lat"].append(station["lat"])
-    data_frame["lon"].append(station["lon"])
-    data_frame["name"].append(station["name"])
-    data_frame["capacity"].append(station["capacity"])
-    data_frame["station_id"].append(station["station_id"])
-df1 = pd.DataFrame(data_frame)
-
-status_data = status()
-df2 = pd.DataFrame(status_data)  
-
-data_frame = df1.merge(df2, on="station_id", how="left")        
-data_frame["color"] = data_frame.apply(calculate_color, axis=1)
-map_data = pd.DataFrame(data_frame)
-
-point_layer = pydeck.Layer(
-    "ScatterplotLayer",
-    data=map_data,
-    id="Stations",
-    get_position=["lon", "lat"],
-    get_color = "color",
-    pickable=True,
-    auto_highlight=True,
-    get_radius="100",
-)
-
-view_state=pydeck.ViewState(
-        latitude=map_data["lat"].mean(), 
-        longitude=map_data["lon"].mean(),
-        zoom=10,
-        pitch=0,
+def create_chart(data, tooltip):
+    point_layer = pydeck.Layer(
+        "ScatterplotLayer",
+        data=data,
+        id="Stations",
+        get_position=["lon", "lat"],
+        get_color = "color",
+        pickable=True,
+        auto_highlight=True,
+        get_radius="100",
     )
 
+    view_state=pydeck.ViewState(
+            latitude=map_data["lat"].mean(), 
+            longitude=map_data["lon"].mean(),
+            zoom=10,
+            pitch=0,
+            bearing=90,
+        )
 
 
-chart = pydeck.Deck(
-    point_layer,
-    initial_view_state=view_state,
-    tooltip={"text": "{name}, \nCapacity: {capacity}, \nAvailable bikes: {bikes}, \nAvailable e-bikes: {ebikes}, \nAvailable scooters: {scooters}"},
-)
+
+    chart = pydeck.Deck(
+        point_layer,
+        initial_view_state=view_state,
+        tooltip=tooltip,
+    )   
+
+    return chart
+
+station_info_data = station_info()
+df1 = pd.DataFrame(station_info_data)
+
+
+on = st.toggle("Return", help ="Click to see the stations accepting returns")
+station_status_data = status_rent()
+df2 = pd.DataFrame(station_status_data)  
+data_frame = df1.merge(df2, on="station_id", how="left")        
+data_frame["color"] = data_frame.apply(lambda row: calculate_color(row, "rent"), axis=1)
+map_data = pd.DataFrame(data_frame)
+tooltip = {"text": "{name}, \nCapacity: {capacity}, \nAvailable bikes: {bikes}, \nAvailable e-bikes: {ebikes}, \nAvailable scooters: {scooters}"}
+chart = create_chart(map_data, tooltip)
+
+if on:
+    st.write("That's where you can return your vehicle. Turn off to get back")
+    station_status_data = status_return()
+    df2 = pd.DataFrame(station_status_data)  
+    data_frame = df1.merge(df2, on="station_id", how="left")        
+    data_frame["color"] = data_frame.apply(lambda row: calculate_color(row, "return"), axis=1)
+    map_data = pd.DataFrame(data_frame)
+    tooltip = {"text": "{name}"}
+    chart = create_chart(map_data, tooltip)
 
 
 event = st.pydeck_chart(chart, on_select="rerun", selection_mode="multi-object")
@@ -73,9 +100,19 @@ event.selection
 
 option = st.selectbox(
     "Detailed overview, sort by:",
-    ("Availability", ""),
+    ("Availability"),
     index=None,
     placeholder="Select filtering option...",
 )
 
-st.dataframe(map_data)
+if option == "Availability":
+    st.dataframe(
+    map_data.assign(percentage=(map_data["bikes"] + map_data["ebikes"] + map_data["scooters"]) / map_data["capacity"] * 100)
+    .sort_values(by="percentage", ascending=True)
+)
+    
+
+else:
+    st.dataframe(map_data, column_config={"color" :None, "station_id" :None,"lat": None, "lon": None })
+
+
--- station_status.py
+++ station_status.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import json
 import requests
-def status():
+def status_rent():
     data_frame = {"station_id": [], "bikes": [], "ebikes": [], "scooters" :[]}
     url = "https://gbfs.lyft.com/gbfs/2.3/chi/en/station_status.json"
     response = requests.get(url)
@@ -24,3 +24,15 @@ def status():
     return data_frame
 
 
+def status_return():
+    data_frame = {"station_id": [], "return": []}
+    url = "https://gbfs.lyft.com/gbfs/2.3/chi/en/station_status.json"
+    response = requests.get(url)
+    
+    data = response.json()
+    for station in data["data"]["stations"]:
+        station_id = station["station_id"]
+        return_status = station["is_returning"]
+        data_frame["station_id"].append(station_id)
+        data_frame["return"].append(return_status)
+    return data_frame
