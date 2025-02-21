0399bec3660d9aee07c2ec9016b5803ae67dae16
unknown <michaljaroch@hotmail.com>
2025-02-21 12:09:59 +0000

    modify sorting methods

--- app.py
+++ app.py
@@ -149,8 +149,8 @@ elif option == "Current location":
     map_data["distance_km"] = map_data.apply(lambda row: geodesic((row["lat"], row["lon"]), user_location).km, axis=1)
 
     #filtered_stations = map_data[(map_data["lat"] == lat) & (map_data["lon"] == lng)]
-    filtered_stations = map_data[map_data["distance_km"] <= 1].sort_values(by="distance", ascending=True)
-    st.dataframe(filtered_stations)
+    filtered_stations = (map_data[map_data["distance_km"] <= 1].sort_values(by="distance", ascending=True))
+    st.dataframe(filtered_stations, column_config={"color" :None, "station_id" :None,"lat": None, "lon": None, "capacity": None})
 
 else:
     st.dataframe(map_data, column_config={"color" :None, "station_id" :None,"lat": None, "lon": None })
